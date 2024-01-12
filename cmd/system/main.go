cf38924a1b84e3c89033b012cd8bef4c54e6d23e
jamersom <jamersom@gmail.com>
2024-01-12 00:36:03 -0300

    implementcao do projeto order list

---
+++ cmd/system/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"net"
+	"net/http"
+
+	graphql_handler "github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jamersom/clean-arch-order-list/configs"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/event/handler"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/graphql/graph"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/grpc/pb"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/grpc/service"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/web/webserver"
+	"github.com/jamersom/clean-arch-order-list/pkg/events"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	// mysql
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rabbitMQChannel := getRabbitMQChannel()
+
+	eventDispatcher := events.NewEventDispatcher()
+
+	eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+	eventDispatcher.Register("OrderList", &handler.OrderListHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+	eventDispatcher.Register("GetOrderByID", &handler.GetOrderByIDHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+	eventDispatcher.Register("OrderUpdate", &handler.OrderUpdateHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+	eventDispatcher.Register("DeleteOrder", &handler.DeleteOrderHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+
+	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+	listOrdersUseCase := NewListOrderUseCase(db, eventDispatcher)
+	getOrderByID := NewGetOrderByIDUseCase(db, eventDispatcher)
+	updateOrderUseCase := NewUpdateOrderUseCase(db, eventDispatcher)
+	deleteOrderUseCase := NewDeleteOrderUseCase(db, eventDispatcher)
+
+	webserver := webserver.NewWebServer(configs.WebServerPort)
+
+	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order", webOrderHandler.Create, "POST")
+
+	webListOrdersHandler := NewWebListOrdersHandler(db, eventDispatcher)
+	webserver.AddHandler("/orders", webListOrdersHandler.List, "GET")
+
+	webGetOrderByIDHandler := NewWebGetOrderByIDHandler(db, eventDispatcher)
+	webserver.AddHandler("/orderbyid", webGetOrderByIDHandler.GetOrderByID, "GET")
+
+	webOrderUpdateHandler := NewWebUpdateOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order/update", webOrderUpdateHandler.UpdateOrder, "POST")
+
+	webDeleteUpdateHandler := NewWebDeleteOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order/delete", webDeleteUpdateHandler.DeleteOrder, "DELETE")
+
+	fmt.Println("Starting web server on port", configs.WebServerPort)
+	go webserver.Start()
+
+	grpcServer := grpc.NewServer()
+
+	orderService := service.NewOrderService(*createOrderUseCase, *listOrdersUseCase, *getOrderByID, *updateOrderUseCase, *deleteOrderUseCase)
+	pb.RegisterOrderServiceServer(grpcServer, orderService)
+
+	reflection.Register(grpcServer)
+
+	fmt.Println("Starting gRPC server on port", configs.GRPCServerPort)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", configs.GRPCServerPort))
+	if err != nil {
+		panic(err)
+	}
+	go grpcServer.Serve(lis)
+
+	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CreateOrderUseCase:  *createOrderUseCase,
+		ListOrderUseCase:    *listOrdersUseCase,
+		GetOrderByIDUseCase: *getOrderByID,
+		UpdateOrderUseCase:  *updateOrderUseCase,
+		DeleteOrderUseCase:  *deleteOrderUseCase,
+	}}))
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	fmt.Println("Starting GraphQL server on port", configs.GraphQLServerPort)
+	http.ListenAndServe(":"+configs.GraphQLServerPort, nil)
+}
+
+func getRabbitMQChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch
+}
---
+++ cmd/system/wire.go
@@ -0,0 +1,151 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+	"github.com/jamersom/clean-arch-order-list/internal/entity"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/database"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/event"
+	"github.com/jamersom/clean-arch-order-list/internal/infra/web"
+	"github.com/jamersom/clean-arch-order-list/internal/usecase"
+	"github.com/jamersom/clean-arch-order-list/pkg/events"
+)
+
+var setOrderRepositoryDependency = wire.NewSet(
+	database.NewOrderRepository,
+	wire.Bind(new(entity.OrderRepositoryInterface), new(*database.OrderRepository)),
+)
+
+var setEventDispatcherDependency = wire.NewSet(
+	events.NewEventDispatcher,
+	event.NewOrderCreated,
+	event.NewOrderList,
+	event.NewGetOrderByID,
+	event.NewOrderUpdate,
+	event.NewDeleteOrder,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+	wire.Bind(new(events.EventInterface), new(*event.OrderList)),
+	wire.Bind(new(events.EventInterface), new(*event.GetOrderByID)),
+	wire.Bind(new(events.EventInterface), new(*event.OrderUpdate)),
+	wire.Bind(new(events.EventInterface), new(*event.DeleteOrder)),
+	wire.Bind(new(events.EventDispatcherInterface), new(*events.EventDispatcher)),
+)
+
+var setOrderCreatedEvent = wire.NewSet(
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+)
+
+var setOrderListedEvent = wire.NewSet(
+	event.NewOrderList,
+	wire.Bind(new(events.EventInterface), new(*event.OrderList)),
+)
+
+var setGetOrderByIDEvent = wire.NewSet(
+	event.NewGetOrderByID,
+	wire.Bind(new(events.EventInterface), new(*event.GetOrderByID)),
+)
+
+var setUpdateOrderEvent = wire.NewSet(
+	event.NewOrderUpdate,
+	wire.Bind(new(events.EventInterface), new(*event.OrderUpdate)),
+)
+
+var setDeleteOrderEvent = wire.NewSet(
+	event.NewDeleteOrder,
+	wire.Bind(new(events.EventInterface), new(*event.DeleteOrder)),
+)
+
+func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.CreateOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		usecase.NewCreateOrderUseCase,
+	)
+	return &usecase.CreateOrderUseCase{}
+}
+
+func NewListOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.ListOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderListedEvent,
+		usecase.NewListOrderUseCase,
+	)
+	return &usecase.ListOrderUseCase{}
+}
+
+func NewGetOrderByIDUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.GetOrderByIDUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setGetOrderByIDEvent,
+		usecase.NewGetOrderByIDUseCase,
+	)
+	return &usecase.GetOrderByIDUseCase{}
+}
+
+func NewUpdateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.UpdateOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setUpdateOrderEvent,
+		usecase.NewUpdateOrderUseCase,
+	)
+	return &usecase.UpdateOrderUseCase{}
+}
+
+func NewDeleteOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.DeleteOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setDeleteOrderEvent,
+		usecase.NewDeleteOrderUseCase,
+	)
+	return &usecase.DeleteOrderUseCase{}
+}
+
+func NewWebOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		web.NewWebOrderHandler,
+	)
+	return &web.WebOrderHandler{}
+}
+
+func NewWebListOrdersHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebListOrdersHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderListedEvent,
+		web.NewWebListOrdersHandler,
+	)
+	return &web.WebListOrdersHandler{}
+}
+
+func NewWebGetOrderByIDHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebGetOrderByIDHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setGetOrderByIDEvent,
+		web.NewWebGetOrderByIDHandler,
+	)
+	return &web.WebGetOrderByIDHandler{}
+}
+
+func NewWebUpdateOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebUpdateOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setUpdateOrderEvent,
+		web.NewWebUpdateOrderHandler,
+	)
+	return &web.WebUpdateOrderHandler{}
+}
+
+func NewWebDeleteOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebDeleteOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setDeleteOrderEvent,
+		web.NewWebDeleteOrderHandler,
+	)
+	return &web.WebDeleteOrderHandler{}
+}
